fcbc7f675a4f4becfa1e0a88445fbe419a8b56b5
maramizo <midoomh@hotmail.com>
2020-12-17 10:31:16 +0200

    Removed unnecessary code.

--- main.py
+++ main.py
@@ -1,38 +1,12 @@
 import discord
 from discord.ext import commands
-
 from dotenv import load_dotenv
-load_dotenv()
-
 import os
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.members = True
-#
-# bot = commands.Bot(command_prefix="#", case_insensitive=True)
-#
-#
-# class MyClient(commands.Cog):
-#     async def on_ready(self):
-#         print('Logged on as {0}!'.format(self.user))
-#
-#     async def on_message(self, message):
-#         print('Message from {0.author}: {0.content}'.format(message))
-#
-#     # Get member join server ID, get channel with specific name in server, send message.
-#     # async def on_member_join(self, member):
-#     #     joined_guild = member.guild
-#     #     send_channel = discord.utils.get(joined_guild.channels, name='newcomers')
-#     #     await client.get_channel(send_channel.id).send(f'**{member}** has joined {member.guild}!')
-#     #
-#     # async def on_member_remove(self, member):
-#     #     left_guild = member.guild
-#     #     send_channel = discord.utils.get(left_guild.channels, name='leavers')
-#     #     await client.get_channel(send_channel.id).send(f'Aww, **{member}** has left. :(')
-#
-#
-# client = MyClient(intents=intents)
-
 
 bot = commands.Bot(command_prefix=".", description="", intents=intents)
 
